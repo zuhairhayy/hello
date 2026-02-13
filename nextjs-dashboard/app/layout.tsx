91b0a01c39fc78bfd059a40dea46d43bba970122
Your Name <your.email@example.com>
2026-02-13 07:13:24 -0800

    initial commit

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,15 @@
+import { inter } from '@/app/ui/fonts';
+
+import '@/app/ui/global.css';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+           <body >{children}</body>
+
+    </html>
+  );
+}
